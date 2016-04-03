32c6f54ddef4c91d3c3e1cf911fff6f08b3a3614
zblash <zblabcefg@163.com>
2016-04-03 11:41:00 +0800

    添加项目文件。

---
+++ ConsoleApplication1/a program deal with math.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	float c, f, n;
+	long line;
+
+	printf("this is war of mine\n");
+	n = getchar(  );
+	f = n;
+		c = (5.0 / 9.0)*(f - 32.0);
+		printf("%3.0f %6.1f\n" ,c, f);
+	
+	system("pause");
+}
+	
